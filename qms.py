5c54d3af6b7907fe83fc4bda31e66ff0fc0b9f23
Alister Brumley <109202180+AlisterBrumley@users.noreply.github.com>
2024-06-26 20:38:35 +1000

    added season type selection

--- qms.py
+++ qms.py
@@ -19,7 +19,7 @@ def auth():
         exit(1)
 
 
-def get_info(mov, db, id):
+def get_info(mov, db, id, season):
     if mov:
         try:
             return db.get_movie(id)
@@ -50,6 +50,57 @@ def get_info(mov, db, id):
             exit(1)
 
 
+def get_season_type(db, id):
+    try:
+        s_types = db.get_series_extended(id)["seasonTypes"]
+    except ValueError:
+        print("ID incorrect or not correct media type!")
+        exit(1)
+    except urllib.error.URLError as e:
+        print("NETWORK ERROR!")
+        print(e)
+        exit(1)
+    except Exception as e:
+        print("UNKNOWN ERROR OCCURED:")
+        print(e)
+        exit(1)
+
+    list_print(s_types)
+
+    list_length = len(s_types)
+    selection = select(list_length)
+    return s_types[selection]["type"]
+
+
+# prints season type list
+def list_print(list):
+    for cnt, dct in enumerate(list):
+        cnt = str(cnt).zfill(2)
+        s_name = dct["name"]
+        s_type = dct["type"]
+
+        print(cnt + ") " + s_name + " - " + s_type)
+
+
+# user selection
+def select(lst_len):
+    selection = input("Selection: ")
+
+    # if no input try again
+    if not selection:
+        return select(lst_len)
+    # if not int try again
+    try:
+        selection = int(selection)
+    except ValueError:
+        return select(lst_len)
+    # if out of range try again
+    if int(selection) >= lst_len:
+        return select(lst_len)
+
+    return selection
+
+
 def main(
     tvdb_id: str,
     movie: Annotated[Optional[bool], typer.Option("--movie", "-m")] = False,
@@ -59,7 +110,14 @@ def main(
     # auth and create class to access database
     tvdb = auth()
 
-    info = get_info(movie, tvdb, tvdb_id)
+    # getting season types and selecting one
+    if not movie:
+        season_type = get_season_type(tvdb, tvdb_id)
+    else:
+        season_type = None
+
+    # getting info, curently not using season type, check notes
+    info = get_info(movie, tvdb, tvdb_id, season_type)
     pprint.pprint(info)
 
 
