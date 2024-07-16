87e29d92d0f5f4144b6bf543021c2d6b1655ad39
Alister Brumley <109202180+AlisterBrumley@users.noreply.github.com>
2024-07-16 13:12:51 +1000

    removed movie, focusing on alternate tv shows

--- qms.py
+++ qms.py
@@ -2,8 +2,8 @@ import tvdb_v4_official
 import typer
 import pprint
 import urllib.error
-from typing_extensions import Annotated
-from typing import Optional
+# from typing_extensions import Annotated
+# from typing import Optional
 
 
 def auth():
@@ -19,35 +19,52 @@ def auth():
         exit(1)
 
 
-def get_info(mov, db, id, season):
-    if mov:
-        try:
-            return db.get_movie(id)
-        except ValueError:
-            print("ID incorrect or not correct media type!")
-            exit(1)
-        except urllib.error.URLError as e:
-            print("NETWORK ERROR!")
-            print(e)
-            exit(1)
-        except Exception as e:
-            print("UNKNOWN ERROR OCCURED:")
-            print(e)
-            exit(1)
-    else:  # if not movie, is series
-        try:
-            return db.get_series(id)
-        except ValueError:
-            print("ID incorrect or not correct media type!")
-            exit(1)
-        except urllib.error.URLError as e:
-            print("NETWORK ERROR!")
-            print(e)
-            exit(1)
-        except Exception as e:
-            print("UNKNOWN ERROR OCCURED:")
-            print(e)
-            exit(1)
+# def get_info_DISABLE(mov, db, id, season):
+#     if mov:
+#         try:
+#             return db.get_movie(id)
+#         except ValueError:
+#             print("ID incorrect or not correct media type!")
+#             exit(1)
+#         except urllib.error.URLError as e:
+#             print("NETWORK ERROR!")
+#             print(e)
+#             exit(1)
+#         except Exception as e:
+#             print("UNKNOWN ERROR OCCURED:")
+#             print(e)
+#             exit(1)
+#     else:  # if not movie, is series
+#         try:
+#             return db.get_series(id)
+#         except ValueError:
+#             print("ID incorrect or not correct media type!")
+#             exit(1)
+#         except urllib.error.URLError as e:
+#             print("NETWORK ERROR!")
+#             print(e)
+#             exit(1)
+#         except Exception as e:
+#             print("UNKNOWN ERROR OCCURED:")
+#             print(e)
+#             exit(1)
+
+
+def get_info(db, id):
+    try:
+        # TODO RETURN MULTIPLE DICTS
+        return db.get_series(id)
+    except ValueError:
+        print("ID incorrect or not correct media type!")
+        exit(1)
+    except urllib.error.URLError as e:
+        print("NETWORK ERROR!")
+        print(e)
+        exit(1)
+    except Exception as e:
+        print("UNKNOWN ERROR OCCURED:")
+        print(e)
+        exit(1)
 
 
 def get_season_type(db, id):
@@ -101,23 +118,30 @@ def select(lst_len):
     return selection
 
 
+# MOVIES DISABLED FOR TIME BEING
 def main(
     tvdb_id: str,
-    movie: Annotated[Optional[bool], typer.Option("--movie", "-m")] = False,
-    # VVV disabled, due to being default and unncesary
+    # movie: Annotated[Optional[bool], typer.Option("--movie", "-m")] = False,
     # series: Annotated[Optional[bool], typer.Option("--series", "-s")] = False
 ):
     # auth and create class to access database
     tvdb = auth()
 
     # getting season types and selecting one
-    if not movie:
-        season_type = get_season_type(tvdb, tvdb_id)
-    else:
-        season_type = None
+    # if not movie:
+    #     season_type = get_season_type(tvdb, tvdb_id)
+    # else:
+    #     season_type = None
 
     # getting info, curently not using season type, check notes
-    info = get_info(movie, tvdb, tvdb_id, season_type)
+    # info = get_info_DISABLE(movie, tvdb, tvdb_id, season_type)
+    # TODO GET MULTIPLE DICTS
+    info = get_info(tvdb, tvdb_id)
+
+    # TODO CREATE CLASSES FROM DICTS
+
+    # TODO CREATE XML FROM CLASSES
+
     pprint.pprint(info)
 
 
