b291bf17ca38ee0f68bad32e51b534c0ed710146
Alister Brumley <109202180+AlisterBrumley@users.noreply.github.com>
2024-07-16 19:22:51 +1000

    changed get_info to return dataclass containing episode info
    also removed disabled code

--- qms.py
+++ qms.py
@@ -1,9 +1,16 @@
 import tvdb_v4_official
 import typer
-import pprint
+import xml
 import urllib.error
-# from typing_extensions import Annotated
-# from typing import Optional
+from dataclasses import dataclass
+# import pprint
+
+
+@dataclass
+class tv_show:
+    ext: dict
+    tra: dict
+    eps: dict
 
 
 def auth():
@@ -19,41 +26,13 @@ def auth():
         exit(1)
 
 
-# def get_info_DISABLE(mov, db, id, season):
-#     if mov:
-#         try:
-#             return db.get_movie(id)
-#         except ValueError:
-#             print("ID incorrect or not correct media type!")
-#             exit(1)
-#         except urllib.error.URLError as e:
-#             print("NETWORK ERROR!")
-#             print(e)
-#             exit(1)
-#         except Exception as e:
-#             print("UNKNOWN ERROR OCCURED:")
-#             print(e)
-#             exit(1)
-#     else:  # if not movie, is series
-#         try:
-#             return db.get_series(id)
-#         except ValueError:
-#             print("ID incorrect or not correct media type!")
-#             exit(1)
-#         except urllib.error.URLError as e:
-#             print("NETWORK ERROR!")
-#             print(e)
-#             exit(1)
-#         except Exception as e:
-#             print("UNKNOWN ERROR OCCURED:")
-#             print(e)
-#             exit(1)
-
-
 def get_info(db, id):
     try:
-        # TODO RETURN MULTIPLE DICTS
-        return db.get_series(id)
+        show = tv_show(
+            db.get_series_extended(id),
+            db.get_series_translation(id, "eng"),
+            db.get_series_episodes(id, "alternate"),
+        )
     except ValueError:
         print("ID incorrect or not correct media type!")
         exit(1)
@@ -66,83 +45,19 @@ def get_info(db, id):
         print(e)
         exit(1)
 
-
-def get_season_type(db, id):
-    try:
-        s_types = db.get_series_extended(id)["seasonTypes"]
-    except ValueError:
-        print("ID incorrect or not correct media type!")
-        exit(1)
-    except urllib.error.URLError as e:
-        print("NETWORK ERROR!")
-        print(e)
-        exit(1)
-    except Exception as e:
-        print("UNKNOWN ERROR OCCURED:")
-        print(e)
-        exit(1)
-
-    list_print(s_types)
-
-    list_length = len(s_types)
-    selection = select(list_length)
-    return s_types[selection]["type"]
+    return show
 
 
-# prints season type list
-def list_print(list):
-    for cnt, dct in enumerate(list):
-        cnt = str(cnt).zfill(2)
-        s_name = dct["name"]
-        s_type = dct["type"]
-
-        print(cnt + ") " + s_name + " - " + s_type)
-
-
-# user selection
-def select(lst_len):
-    selection = input("Selection: ")
-
-    # if no input try again
-    if not selection:
-        return select(lst_len)
-    # if not int try again
-    try:
-        selection = int(selection)
-    except ValueError:
-        return select(lst_len)
-    # if out of range try again
-    if int(selection) >= lst_len:
-        return select(lst_len)
-
-    return selection
-
-
-# MOVIES DISABLED FOR TIME BEING
-def main(
-    tvdb_id: str,
-    # movie: Annotated[Optional[bool], typer.Option("--movie", "-m")] = False,
-    # series: Annotated[Optional[bool], typer.Option("--series", "-s")] = False
-):
+def main(tvdb_id: str):
     # auth and create class to access database
     tvdb = auth()
 
-    # getting season types and selecting one
-    # if not movie:
-    #     season_type = get_season_type(tvdb, tvdb_id)
-    # else:
-    #     season_type = None
-
-    # getting info, curently not using season type, check notes
-    # info = get_info_DISABLE(movie, tvdb, tvdb_id, season_type)
-    # TODO GET MULTIPLE DICTS
+    # gets info from database returns in dataclass
     info = get_info(tvdb, tvdb_id)
 
-    # TODO CREATE CLASSES FROM DICTS
+    # TODO CREATE XML FROM CLASS
 
-    # TODO CREATE XML FROM CLASSES
-
-    pprint.pprint(info)
+    # pprint.pprint(info.ext["name"])
 
 
 if __name__ == "__main__":
