8cffc67f9b478f9e2221a37098e6d1ca191d2212
Alister Brumley <ali.b912@gmail.com>
2024-07-26 23:35:06 +1000

    added artwork downloader - still in progress

--- quickMediaScrape.py
+++ quickMediaScrape.py
@@ -1,6 +1,8 @@
 # BUILT IN IMPORTS
 import xml.etree.ElementTree as ET
+import urllib.request
 from pathlib import Path
+from dataclasses import dataclass
 
 # DEPENDENCY IMPORTS
 import typer
@@ -8,6 +10,16 @@ import typer
 # LOCAL IMPORTS
 import helpers.makexml as make
 import helpers.dbget as get
+
+
+@dataclass
+class filecounts:
+    banner: int = 0
+    poster: int = 0
+    fanart: int = 0
+    clrArt: int = 0
+    clrLogo: int = 0
+    unknown: int = 0
 
 
 def output_xml(el_tree, out_path):
@@ -33,6 +45,47 @@ def ep_xml_loop(inf, ep_filelist, act_tree):
         output_xml(ep_xml, ep_out_path)
 
 
+def art_download(art_inf, series_path):
+    # filename counters
+    cnt = filecounts()
+    series_path = series_path / "images"
+    season_cnt = {}  # TODO
+
+    answer = input("Do you want to download artwork(y/N)? ").lower().strip()
+    if answer != "y":
+        return False
+
+    for art in art_inf:
+        match art["type"]:
+            case 1:  # banner
+                filename = series_path / ("banner-" + str(cnt.banner).zfill(2) + ".jpg")
+                cnt.banner += 1
+            case 2:  # poster
+                filename = series_path / ("poster-" + str(cnt.poster).zfill(2) + ".jpg")
+                cnt.poster += 1
+            case 3:  # fanart/background
+                filename = series_path / ("fanart-" + str(cnt.fanart).zfill(2) + ".jpg")
+                cnt.fanart += 1
+            case 7:  # season
+                continue
+            case 22:  # clearart
+                filename = series_path / (
+                    "clearart-" + str(cnt.clrArt).zfill(2) + ".jpg"
+                )
+                cnt.clrArt += 1
+            case 23:  # clearlogo
+                filename = series_path / (
+                    "clearlogo-" + str(cnt.clrLogo).zfill(2) + ".jpg"
+                )
+                cnt.clrLogo += 1
+            case _:  # unknown
+                filename = series_path / (
+                    "UNKNOWN-" + str(cnt.unknown).zfill(2) + ".jpg"
+                )
+                cnt.unknown += 1
+        urllib.request.urlretrieve(art["image"], filename)
+
+
 def main(tvdb_id: str, dir_path: Path):
     # auth and create var to access database
     tvdb = get.auth()
@@ -54,6 +107,9 @@ def main(tvdb_id: str, dir_path: Path):
     episode_filelist = get_files(dir_path)
     # creating and writing episode loop
     ep_xml_loop(info, episode_filelist, actor_tree)
+
+    # asking if user wants to download art
+    art_download(info.ext["artworks"], dir_path)
 
 
 if __name__ == "__main__":
